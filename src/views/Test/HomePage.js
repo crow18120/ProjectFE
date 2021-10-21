21c4b8816e4d66e52435e465732893bd8971a6c1
crow18120 <70528185+crow18120@users.noreply.github.com>
2021-10-22 01:21:37 +0700

    Update HomePage.js

--- src/views/Test/HomePage.js
+++ src/views/Test/HomePage.js
@@ -1,19 +1,15 @@
 import React from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
-
-import { Link } from "react-router-dom";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 // react-router-dom
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 // @material-ui/icons
-// import Camera from "@material-ui/icons/Camera";
-// import Palette from "@material-ui/icons/Palette";
-// import Favorite from "@material-ui/icons/Favorite";
+
 // core components
 import Header from "components/Header/Header.js";
-// import Footer from "components/Footer/Footer.js";
+import Footer from "components/Footer/Footer.js";
 import Button from "components/CustomButtons/Button.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
@@ -122,6 +118,7 @@ export default function ProfilePage(props) {
           ))}
         </GridContainer>
       </div>
+      <Footer />
     </div>
   );
 }
